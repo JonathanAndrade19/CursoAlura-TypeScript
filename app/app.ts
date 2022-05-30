c4a1e532a23633a84d7090e162c9c921cb274b20
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-05-29 21:43:10 -0300

    modifying tsconfig.json file

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,6 @@
-import { Negociacao } from './models/negociacao';
+import { Negociacao } from './models/negociacao.js';
 
 const negociacao = new Negociacao(new Date(), 10, 100);
 
-console.log(negociacao.data);
-console.log(negociacao.quantidade);
-console.log(negociacao.valor);
 console.log(negociacao.volume);
+console.assert(negociacao.data);
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,4 @@
-import { Negociacao } from './models/negociacao';
+import { Negociacao } from './models/negociacao.js';
 const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.data);
-console.log(negociacao.quantidade);
-console.log(negociacao.valor);
 console.log(negociacao.volume);
+console.assert(negociacao.data);
